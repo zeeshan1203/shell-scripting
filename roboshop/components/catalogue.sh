3a9bcc3afb80bb63e923516a2e67e01aae5c2e19
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-15 22:24:53 +0530

    script for catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -30,7 +30,7 @@ HEAD "Install NodeJS Dependencies\t"
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
 STAT $?
 
-HEAD "Fix Permissions to App Content\t"
+HEAD "Fix Permissions to App Content"
 chown roboshop:roboshop /home/roboshop -R
 STAT $?
 
