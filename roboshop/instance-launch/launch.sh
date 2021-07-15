34b51393ccdaa922a48a47a4010244cfe280e504
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-15 19:03:37 +0530

    script modified launch.sh

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -8,13 +8,13 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-LID=lt-0d29253072b5c5d49
-LVER=1
+LID=lt-090f6832097248547
+LVER=2
 
-## Validate if instance is already there
+## Validate If Instance is already there
 
-INSTANCE_CREATED() {
-  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | xargs -n1)
+INSTANCE_CREATE() {
+  INSTANCE_STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}"  | jq .Reservations[].Instances[].State.Name | xargs -n1)
   if [ "${INSTANCE_STATE}" = "running" ]; then
     echo "${COMPONENT} Instance already exists!!"
     exit 0
@@ -25,6 +25,6 @@ INSTANCE_CREATED() {
     exit 0
   fi
 
-  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
 }
