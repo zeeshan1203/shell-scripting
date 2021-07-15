29ab3cc43963f44e90a18071e73fbd6e82d3213c
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-15 21:40:58 +0530

    script for catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -4,3 +4,39 @@ source components/common.sh
 rm -f /tmp/roboshop.log
 set-hostname catalogue
 
+HEAD "Install NodeJS\t\t"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download App From GitHub\t"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the Downloaded Archive\t"
+cd /home/roboshop && rm -rf catalogue && unzip /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "Install NodeJS Dependencies\t"
+cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Add Roboshop App User\t\t"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there,So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi
+
+HEAD "Fix Permissions to App Content\t\t"
+chown roboshop:roboshop /home/roboshop -Roboshop
+STAT $?
+
+# HEAD "Start Catalogue Service"
+# sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/'
+# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# systemctl daemon-reload
+# systemctl start catalogue
+# systemctl enable catalogue
