c05ade5f71ff51ad13e209f3206d1dbbd60db73c
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-15 22:21:12 +0530

    script for catalogue.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -8,6 +8,16 @@ HEAD "Install NodeJS\t\t"
 yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Add Roboshop App User\t\t"
+id roboshop &>>/tmp/roboshop.log
+if [ $? -eq 0 ]; then
+  echo User is already there,So Skipping the User creation &>>/tmp/roboshop.log
+  STAT $?
+else
+  useradd roboshop &>>/tmp/roboshop.log
+  STAT $?
+fi
+
 HEAD "Download App From GitHub\t"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
 STAT $?
@@ -19,16 +29,6 @@ STAT $?
 HEAD "Install NodeJS Dependencies\t"
 cd /home/roboshop/catalogue && npm install --unsafe-perm &>>/tmp/roboshop.log
 STAT $?
-
-HEAD "Add Roboshop App User\t\t"
-id roboshop &>>/tmp/roboshop.log
-if [ $? -eq 0 ]; then
-  echo User is already there,So Skipping the User creation &>>/tmp/roboshop.log
-  STAT $?
-else
-  useradd roboshop &>>/tmp/roboshop.log
-  STAT $?
-fi
 
 HEAD "Fix Permissions to App Content\t\t"
 chown roboshop:roboshop /home/roboshop -Roboshop
