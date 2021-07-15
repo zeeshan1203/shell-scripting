80f94277097562be90bbdacc07c46be9ef12098d
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-15 18:58:03 +0530

    script modified launch.sh

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -2,8 +2,8 @@
 
 COMPONENT=$1
 
-## -z validates the variable empty, true if it is empty.
-if [ -z "${COMPONENT}"]; then
+## -z validates the variable empty , true if it is empty.
+if [ -z "${COMPONENT}" ]; then
   echo "Component Input is Needed"
   exit 1
 fi
