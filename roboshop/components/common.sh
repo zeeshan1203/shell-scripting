61815c9439a3b82d28f32c56e460c488b5205a0e
Zeeshan Shaik <zeeshanshaik1203@gmail.com>
2021-07-16 16:08:29 +0530

    script modifies in common.sh

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -33,7 +33,7 @@ SETUP_SYSTEMD() {
   sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' -e 's/CARTENDPOINT/cart.roboshop.internal/' -e 's/DBHOST/mysql.roboshop.internal/' -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabbitmq.roboshop.internal/' /home/roboshop/$1/systemd.service  && mv /home/roboshop/$1/systemd.service /etc/systemd/system/$1.service
   STAT $?
 
-  HEAD "Start $1 Service\t\t"
+  HEAD "Start $1 Service\t"
   systemctl daemon-reload && systemctl enable $1 &>>/tmp/roboshop.log && systemctl restart $1 &>>/tmp/roboshop.log
   STAT $?
 }
