91bec5cd6a6c2bb1231d2253bca8bbe534620352
zeeshan1203 <zeeshanshaik1203@gmail.com>
2021-12-31 23:45:46 +0530

    added & updated and modified

--- roboshop/instance-launch/launch.sh
+++ roboshop/instance-launch/launch.sh
@@ -8,15 +8,15 @@ if [ -z "${COMPONENT}" ]; then
   exit 1
 fi
 
-LID=lt-0c749183079899d8c
-LVER=2
+LID=lt-0c749183079899d8c                    ##ur aws launch template id
+LVER=2                                      ##ur aws launch template latest version
 
 ## Validate If Instance is already there
 
 DNS_UPDATE() {
   PRIVATEIP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | xargs -n1)
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${PRIVATEIP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z0455275BOCGUWER4BVO --change-batch file:///tmp/record.json | jq
+  aws route53 change-resource-record-sets --hosted-zone-id Z0455275BOCGUWER4BVO --change-batch file:///tmp/record.json | jq         ##ur route53 hosted zone id
 }
 
 INSTANCE_CREATE() {
