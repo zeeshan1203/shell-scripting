2af5bbcf27fdf9c3b4fa4a4dc73e860237cba0a2
zeeshan1203 <zeeshanshaik1203@gmail.com>
2022-01-27 18:13:10 +0530

    for practice and understand

--- 02-print.sh
+++ 02-print.sh
@@ -10,16 +10,16 @@ echo Hello World
 # echo to print multiple lines (\n is new line sequence)
 # Syntax : echo -e "line1\nline2"
 
-echo -e "Hello,\nWelcome to DevOps training"
-echo -e "\nHello,\n\nWelcome to DevOps training"
+echo -e "Hello,\nWelcome to DevOps training\n"
+echo -e "\nHello,\n\nWelcome to DevOps training\n"
 
 ## Print a paragraph
 ## \t is to print a tab space
-echo -e "\tThe storm, which had weakened during the afternoon,
+echo -e "\n\tThe storm, which had weakened during the afternoon,
         had left the coastal states on edge, with the Bengal
         government claiming that at least one crore people have
         been affected by the calamity thus far, as it hollered
-        on its destructive path towards Jharkhand."
+        on its destructive path towards Jharkhand\n."
 
 
 ## Print Text in Colors
